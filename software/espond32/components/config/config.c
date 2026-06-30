fd05332af6f3b9b6bbdfd4309e0df8aee0951229
nhrobertson <nhrobertson4@gmail.com>
2026-06-29 23:54:54 -0400

    added cJSON dependency

--- software/espond32/components/config/config.c
+++ software/espond32/components/config/config.c
@@ -1,3 +1,27 @@
 #include "config.h"
 
-esp_err_t parse_config_json();
+/**
+ *  Json Format:
+ *  {
+ *    "version": X,
+ *    "outputs": [
+ *    { "name": "pump1", "on": "XX:XX", "off": "XX:XX", "days": [0-6,x7] },
+ *    { "name": "pump2", "on": "XX:XX", "off": "XX:XX", "days": [0-6,x7] },
+ *    { "name": "valve1", "on": "XX:XX", "off": "XX:XX", "days": [0-6,x7] },
+ *    { "name": "lights1", "on": "XX:XX", "off": "XX:XX", "days": [0-6,x7] },
+ *    ],
+ *    "float_sens": { "threshold_min": 10, "overflow_min": 30, "max_fills_per_day": 3}
+ *  }
+ *  
+ */
+esp_err_t parse_config_json(char *config_str) {
+  cJSON *root = cJSON_Parse(config_str);
+  if (root == NULL) {
+    const char *error_ptr = cJSON_GetErrorPtr();
+    if (error_ptr != NULL) {
+      return ESP_ERR_INVALID_RESPONSE;
+    }
+  
+
+  }
+}
